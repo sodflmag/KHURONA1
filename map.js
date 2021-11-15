c7902e775fcd979c91e3467f96d430599fb38027
woosung <dntjd991223khu.ac.kr>
2021-11-15 21:51:45 +0900

    Add pop-up by region

--- map.js
+++ map.js
@@ -1,22 +1,3 @@
-// const x = document.querySelector("#CD50");
-// const x2 = document.querySelector("#TCD50");
-
-// x.onmouseenter = function() {
-//     x.classList.add("mouseEnter");
-// }
-
-// x.onmouseleave = function() {
-//     x.classList.remove("mouseEnter");
-// }
-
-// x2.onmouseenter = function() {
-//     x.classList.add("mouseEnter");
-// }
-
-// x2.onmouseleave = function() {
-//     x.classList.remove("mouseEnter");
-// }
-
 // 각각의 path와 text에 대해서  커서를 올렸을 시 이펙트 구현
 const paths = document.getElementsByTagName("path");
 const texts = document.getElementsByTagName("text");
@@ -39,9 +20,9 @@ for (let i = 0; i < length; i++) {
     }
 }
 
-// distance_val은 백엔드로부터 받아와야 함, 지역 별 거리두기 정보
-// mapping 방식은 인덱스 기준. 
 const distance_val = [1,2,2,3,1,2,4,2,3,2,4,2,1,3,4,3,2];
+// distance_val은 백엔드로부터 받아와야 함, 지역 별 거리두기 단계 정보
+// mapping 방식은 인덱스 기준. 
 
 for (let i = 0; i < length; i++) {
     if(distance_val[i] != 1) {
@@ -52,3 +33,16 @@ for (let i = 0; i < length; i++) {
     }
 }
 
+// 각 지역별로 eventhandler를 추가, 지역 클릭 시 추가적인 팝업이 나타나도록 구현.
+
+  for (let i = 0; i < length; i++) {
+      paths[i].addEventListener("click", () => {
+          document.querySelector(".area" + i).classList.add("show");
+      })
+      texts[i].addEventListener("click", () => {
+        document.querySelector(".area" + i).classList.add("show");
+    })
+      document.querySelector("#close" + i).addEventListener("click", () => {
+          document.querySelector(".area" + i).classList.remove("show");
+      })
+  }
