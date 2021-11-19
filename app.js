0374e2aa52adc3afe176b83ec54635a2763703c9
woosung <dntjd991223khu.ac.kr>
2021-11-19 21:51:59 +0900

    Apply COVID count for each area by using API

--- app.js
+++ app.js
@@ -4,24 +4,16 @@ app.use(express.static('public'));
 // static 선언
 app.set('view engine', 'ejs');
 
-// app.listen(8080, () => {
-//     console.log("listening port 8080");
-// });
-
-app.get('/', (req, res) => {
-    res.render('test.ejs', {'email': 'hello'});
-});
-
 const request = require("request");
 const convert = require("xml-js");
 const date = new Date();
 const year = date.getFullYear();
-const month = date.getMonth() + 1;
-const day = date.getDate() - 1; // 전일을 기준으로 함
-
+const month = date.getMonth() + 1 > 10 ? date.getMonth() + 1 : '0' + date.getMonth();
+const day = date.getDate() > 10 ? date.getDate() : '0' + date.getDate(); 
+// month, date의 경우 10보다 작아지는 경우 앞에 0을 붙여줘야 정상적인 URL 생성이 가능함.
 const url = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?";
 const key = "680%2FgyPOpacfjvTYIO1H2rHe4F%2FfdZV1jVBWfNkVZt8XL9zsYRp%2BbIO2%2FGwKolSMM6RcViMLiPgq4S%2BQJhROFQ%3D%3D";
-const pageNo = "2";
+const pageNo = "1";
 const numofRows = "10";
 const startCreateDt = `${year}${month}${day}`;
 const endCreateDt = `${year}${month}${day}`;
@@ -43,12 +35,49 @@ function getData() {
 
 // json 형식의 파일 받아옴.
 getData().then(function(data) {
-    const obj = JSON.parse(data);
-    // console.log(obj["response"]["body"]["items"]["item"]);
-    // console.log(data);
+    return new Promise(function(resolve, reject) {
+        let values = [];
+        const obj = JSON.parse(data);
     for (x of obj["response"]["body"]["items"]["item"]) {
-        console.log(x['gubun']['_text']);
+        const areaName = x['gubun']['_text'];
+        const areaCovidCount = x['localOccCnt']['_text'];
+        values.push({'areaName' : areaName, 
+            'areaCovidcount' : areaCovidCount,
+    })
     }
-}).catch(function(err) {
-    console.log(err);
-});
+    resolve(values); // values에는 객체 형태로 지역 별 정보 저장.
+    }
+    )}).then((result) => {
+        result = result.reverse();
+        // console.log(result);
+        // 1번부터 서울, 17번 제주까지.
+        app.get('/', (req, res) => {
+            res.render('view.ejs', {distancingval1: result[1].areaCovidcount,
+                                    distancingval2: result[2].areaCovidcount,
+                                    distancingval3: result[3].areaCovidcount,
+                                    distancingval4: result[4].areaCovidcount,
+                                    distancingval5: result[5].areaCovidcount,
+                                    distancingval6: result[6].areaCovidcount,
+                                    distancingval7: result[7].areaCovidcount,
+                                    distancingval8: result[8].areaCovidcount,
+                                    distancingval9: result[9].areaCovidcount,
+                                    distancingval10: result[10].areaCovidcount,
+                                    distancingval11: result[11].areaCovidcount,
+                                    distancingval12: result[12].areaCovidcount,
+                                    distancingval13: result[13].areaCovidcount,
+                                    distancingval14: result[14].areaCovidcount,
+                                    distancingval15: result[15].areaCovidcount,
+                                    distancingval16: result[16].areaCovidcount,
+                                    distancingval17: result[17].areaCovidcount,
+                                    date : `${year}.${month}.${day}`
+                                    
+        });
+        });
+        
+    })
+
+
+
+    app.listen(8080, () => {
+        console.log("listening port 8080");
+    });
--- public/map.js
+++ public/map.js
@@ -20,21 +20,30 @@ for (let i = 0; i < length; i++) {
     }
 }
 
-const distance_val = [1,2,2,3,1,2,4,2,3,2,4,2,1,3,4,3,2];
+const distance_val = [];
 // distance_val은 API로부터 받아오게 됨, length = 16
-
+// 백앤드에서 받은 API 데이터를 어떻게 Frontend 부분 JS에서 처리할지 -> EJS로 ID 값에 확진자 수 설정. ID는 어차피 쓰이지 않음.
  // 서울 부산 대구 인천 광주 대전 울산 세종 경기도 강원도 충청북도 충청남도 전라북도 전라남도 경상북도 경상남도 제주도 순 인덱스 부여
 
-  
-
-for (let i = 0; i < length; i++) {
-    if(distance_val[i] != 1) {
-        if(paths[i].classList.contains("CD")) {
-            let temp_string = "distancelvl" + distance_val[i];
-            paths[i].classList.add(temp_string);
-        }
+ for (let i = 0; i < length; i++) {
+    const temp_for_path = paths[i];
+    let weight = 0;
+    const IDInteger = parseInt(temp_for_path.id);
+    if(IDInteger< 30) {
+        weight = 1;
+    } else if(IDInteger < 60) {
+        weight = 2;
+    } else if(IDInteger < 90) {
+        weight = 3;
+    } else {
+        weight = 4;
+    }
+    if(paths[i].classList.contains("CD")) {
+        let temp_string = "distancelvl" + weight;
+        paths[i].classList.add(temp_string);
     }
 }
+  
 
 // 각 지역별로 eventhandler를 추가, 지역 클릭 시 추가적인 팝업이 나타나도록 구현.
 
